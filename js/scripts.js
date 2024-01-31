c652712d0fc5fc5a124a3a667027a9ab09b64966
jeremybarish <156790622+jeremybarish@users.noreply.github.com>
2024-01-31 15:52:40 -0500

    adding javascript

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+
+//This is a single line comment, started with two slashes
+/* This is a multi-line comment
+Everything in here has no effect.
+var test = 'This will not actually create a variable';
+*/
+console.log("Hello World")
