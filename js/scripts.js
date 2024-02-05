23a9019d9cc629b3f278e6e0689897cf14bc81f5
jeremybarish <156790622+jeremybarish@users.noreply.github.com>
2024-02-05 17:34:37 -0500

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 
-//This is a single line comment, started with two slashes
-/* This is a multi-line comment
-Everything in here has no effect.
-var test = 'This will not actually create a variable';
-*/
-console.log("Hello World")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
